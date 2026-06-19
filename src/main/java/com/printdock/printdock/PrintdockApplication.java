934d9157722d552ce9a513abfc62d45377804af9
Airel Adrivano <165012306+theoneandonlyvabo@users.noreply.github.com>
2026-06-19 21:19:48 +0700

    Spring Boot Project Setup @Printdock

---
+++ src/main/java/com/printdock/printdock/PrintdockApplication.java
@@ -0,0 +1,13 @@
+package com.printdock.printdock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintdockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintdockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/printdock/printdock/PrintdockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.printdock.printdock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintdockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
