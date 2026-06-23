0d1f6f3d88c70a5afe66ee47da55c8132b9fd689
Airel Adrivano <165012306+theoneandonlyvabo@users.noreply.github.com>
2026-06-23 19:10:34 +0700

    feat: add custom exceptions and GlobalExceptionHandler

---
+++ src/main/java/com/printdock/printdock/exception/KategoriMasihDigunakanException.java
@@ -0,0 +1,7 @@
+package com.printdock.printdock.exception;
+
+public class KategoriMasihDigunakanException extends RuntimeException {
+    public KategoriMasihDigunakanException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/printdock/printdock/exception/KategoriNotFoundException.java
@@ -0,0 +1,7 @@
+package com.printdock.printdock.exception;
+
+public class KategoriNotFoundException extends RuntimeException {
+    public KategoriNotFoundException(String message) {
+        super(message);
+    }
+}
